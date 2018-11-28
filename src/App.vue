54f24746c11df78c9a994dcc59c24e34d7c960c7
Quang Tran <logan1011001@gmail.com>
2018-11-28 12:07:47 +0700

    :zap:Update route name for Article Detail

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@ export default {
         const innerHeight = window.innerHeight;
         if (
           innerHeight + scrollY >= offsetHeight - 500 &&
-          (this.$route.name === "article" || this.$route.name === "home")
+          (this.$route.name === "article-detail" || this.$route.name === "home")
         ) {
           const catId = this.$store.state.categories.selectedCat.id;
           this.$store.dispatch("articles/fetchCatArticles", catId);
